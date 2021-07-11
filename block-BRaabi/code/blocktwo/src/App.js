76fa2b92e00319e0da39a23d9876e4e4c4d60ff5
AaronR <beingbetter21@gmail.com>
2021-07-12 04:21:09 +0530

    error found and resolved

--- block-BRaabi/code/blocktwo/src/App.js
+++ block-BRaabi/code/blocktwo/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import data from './data'
+import data from './data';
 import './App.css';
 import Console from './components/console'
 import Button from './components/button'
@@ -9,25 +9,29 @@ class App extends React.Component{
     super()
     this.clicked=this.clicked.bind(this)
     this.state={
-      Questions:data
+      Questions:data//array of objects
     }
   }
   clicked(i){
     this.setState({
       Questions:this.state.Questions.map((question,index)=>{
-        if(index==i){return question.open=!question.open}
+        console.log(question,'within map');
+        if(index==i){question.open=!question.open}
+        return question;
       })
     })
-    console.log(this.state.Questions,"AFTER CLICK");console.log("AFTER CLICK");
   }
 render(){
   return(
     <>
     {
+      console.log(this.state.Questions,"main")
+    }
+    {
     this.state.Questions.map((question,index)=>(
       <>
-           <Console log={question} key={index*10}/>
-           <Button fn={this.clicked} a={question.A} q={question.Q} o={question.open} i={index}/>
+           <Console log={question} key={index*1.2+1}/>
+           <Button key={index+1.1} fn={this.clicked} a={question.A} q={question.Q} o={question.open} i={index}/>
       </>
     ))
     }
